8a87246045db82341f1f25be2c2fba4f01ce3b64
Dhruv-Sapra <111555972+Dhruv-Sapra@users.noreply.github.com>
2023-02-10 01:43:44 +0530

    updated code

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import cv2
-import cvzone
-from cvzone.HandTrackingModule import HandDetector
+import Handtrackingmoduleadv as htm
 import numpy as np
+from pathlib import Path
 
 puckPos = [100, 100]
 speedX = 5
@@ -10,17 +10,18 @@ gameOver = False
 score = [0, 0]
 
 
-Background = cv2.imread(r"C:\\Users\\LEGION\Desktop\\AllDesktop\\Inheritance\\Ping Pong Game\\4729668.png",cv2.IMREAD_UNCHANGED)
+path = str(Path.cwd())
+Background = cv2.imread(path +"\\images\\4729668.png",cv2.IMREAD_UNCHANGED)
 Background = cv2.resize(Background, (1280,720))
-PaddleLeft = cv2.imread(r"C:\\Users\\LEGION\\Desktop\\AllDesktop\\Inheritance\\Ping Pong Game\\unnamed.png",cv2.IMREAD_UNCHANGED)
+PaddleLeft = cv2.imread(path + "\\images\\unnamed.png",cv2.IMREAD_UNCHANGED)
 PaddleLeft =cv2.resize(PaddleLeft,(100,100))
-PaddleRight = cv2.imread(r"C:\\Users\\LEGION\\Desktop\\AllDesktop\\Inheritance\\Ping Pong Game\\unnamed.png",cv2.IMREAD_UNCHANGED)
+PaddleRight = cv2.imread(path + "\\images\\unnamed.png",cv2.IMREAD_UNCHANGED)
 PaddleRight =cv2.resize(PaddleRight,(100,100))
-Puck =cv2.imread(r"C:\Users\LEGION\Desktop\AllDesktop\Inheritance\Ping Pong Game\share-it-green-glowing-circle-11563079981sivw5upspu.png",cv2.IMREAD_UNCHANGED)
+Puck =cv2.imread(path + "\\images\\share-it-green-glowing-circle-11563079981sivw5upspu.png",cv2.IMREAD_UNCHANGED)
 Puck =cv2.resize(Puck,(80,80))
 
 
-detector = HandDetector(detectionCon=0.8,maxHands=2)
+detector = htm.HandDetector(detectionCon=0.8,maxHands=2)
 
 cap = cv2.VideoCapture(0)
 cap.set(3,1280)
@@ -53,7 +54,7 @@ while True:
             
  
             if hand['type'] == "Left":
-                img = cvzone.overlayPNG(img, PaddleLeft, (x1, y1))
+                img = htm.overlayPNG(img, PaddleLeft, (x1, y1))
                 if x1-w1//2 < puckPos[0] < x1 + w1//2 and y1-h1//2 < puckPos[1] < y1 + h1//2:
                     speedX = -speedX
                     speedX +=5
@@ -61,7 +62,7 @@ while True:
                     score[0] += 1
 
             if hand['type'] == "Right":
-                img = cvzone.overlayPNG(img, PaddleRight, (x2, y2))
+                img = htm.overlayPNG(img, PaddleRight, (x2, y2))
                 if x2-w2//2 < puckPos[0] < x2 +w2//2 and y2-h2//2 < puckPos[1] < y2 + h2//2:
                     speedX = -speedX
                     speedX -= 5
@@ -78,7 +79,7 @@ while True:
         gameOver = True
  
     if gameOver:
-        imgGameOver = cv2.imread(r"C:\Users\LEGION\Desktop\AllDesktop\Inheritance\Ping Pong Game\gameOver.png")
+        imgGameOver = cv2.imread(path + "\\images\\gameOver.png")
         imgGameOver = cv2.resize(imgGameOver, (1280,720))
         img = imgGameOver
         # cv2.putText(img, str(score[1] + score[0]).zfill(2), (585, 360), cv2.FONT_HERSHEY_COMPLEX,
@@ -95,7 +96,7 @@ while True:
         puckPos[1] += speedY
  
         # Draw the ball
-        img = cvzone.overlayPNG(img, Puck, puckPos)
+        img = htm.overlayPNG(img, Puck, puckPos)
  
         cv2.putText(img, str(score[0]), (300, 650), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 255, 255), 5)
         cv2.putText(img, str(score[1]), (900, 650), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 255, 255), 5)
